5439366d35fefd2323252e1dfb9cd7a905372031
faridmusayev <mfarid994@gmail.com>
2021-09-07 20:35:39 +0200

    update euc.R with help documentation

--- R/euc.R
+++ R/euc.R
@@ -1,6 +1,22 @@
 
 
 
+#' Euclidean Algorithm
+#'
+#' The goal of this algorithm is to find the greatest common divisor of two input (a,b) numbers
+#' @param a Must be numeric scalar or integer
+#' @param b Must be numeric scalar or integer
+#'
+#' @return The greatest common divisor of \code{a} and \code{b}
+#' @export
+#'
+#' @examples
+#' euc(100,10)
+#'
+#' \dontrun{
+#' euc('dog','cat')
+#' }
+#'
 euc<-function(a,b){
 
   if (!is.numeric(a) | !is.numeric(b)) stop('Wrong input')
