ab9af4cc78fbb3236bcbbc9caed3784f1841f731
faridmusayev <mfarid994@gmail.com>
2021-09-12 22:46:42 +0200

    upload wiki_graph dataset

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#'Dataset for Dijkstra Algorithm
+#'
+#'A dataset contains corresponding vertices and their weights.
+#'
+#'
+#'@format A data frame with 18 rows and 3 variables:
+#'\describe{
+#'    \item{v1}{1st vertex}
+#'    \item{v2}{2nd vertex}
+#'    \item{w}{weight between two vertices}
+#'}
+#'
+#'@source \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+"wiki_graph"
