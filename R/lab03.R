c577eb452af9397c7b724e7c21ba92c0e4aa0a48
faridmusayev <mfarid994@gmail.com>
2021-09-12 22:48:31 +0200

    package documentation

---
+++ R/lab03.R
@@ -0,0 +1,27 @@
+#' Package with Euclidean and Dijkstra's algorithms
+#'
+#'Two functions are included:
+#' \code{euclidean} and \code{dijkstra}
+#'
+#'@details
+#'
+#'For further information check functions' documentation.
+#'
+#'@author
+#'
+#'- Farid Musayev
+#'- Mucahit Sahin
+#'- Selen Karaduman
+#'
+#'@seealso
+#'
+#'Useful links:
+#'
+#'\url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
+#'
+#'\url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+#'
+#' @docType package
+#' @name lab03
+NULL
+#> NULL
